a2d6c05fee5c2c93ce3d224b74a80658095eb36c
Haritha Kodathala <kharithar@gmail.com>
2018-11-20 10:18:27 -0800

    Add files via upload
    AdventOfCodeDay11

---
+++ Solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+public class Solution {
+	
+	public static void main(String[] args) throws Exception{
+		RTG rtg = new RTG();
+		//RTG.State state = rtg.new State(0, Arrays.asList(0,0,0,0,2,1,1,1,1,1));
+		RTG.State state = rtg.new State(0, Arrays.asList(0,1,0,2));
+		try{
+		List<RTG.State> steps = rtg.findMinSteps(state);
+		visualizeSolution(steps);
+		System.out.println("No. of steps taken to reach F4: "+ (steps.size()-1));
+		}catch(Exception e) {
+			System.out.println(e);
+		}	
+	}
+	
+	public static void visualizeSolution(List<RTG.State> steps) {
+	    steps.forEach(state -> {
+	        try {
+				Thread.sleep(150);
+				System.out.println("Current elevator position : "+ state.floor + " State : "+state.items);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	    });
+	}
+	
+}
